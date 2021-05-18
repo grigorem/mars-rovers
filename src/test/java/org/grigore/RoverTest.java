473a945b1c47e4d7ebf9e55302402a449bbc3f46
Marius Grigore <mariusgrygore@gmail.com>
2021-05-18 14:14:16 +0100

    Created basic tests for Rover class

---
+++ src/test/java/org/grigore/RoverTest.java
@@ -0,0 +1,36 @@
+package org.grigore;
+
+import junit.framework.TestCase;
+
+public class RoverTest extends TestCase {
+
+    public void testMove() {
+        Rover rover = new Rover(1, 1, 'N');
+        assertTrue(rover.move());
+    }
+
+    public void testTurnLeft() {
+        Rover rover = new Rover(1, 1, 'N');
+        assertTrue(rover.turnLeft());
+    }
+
+    public void testTurnRight() {
+        Rover rover = new Rover(1, 1, 'N');
+        assertTrue(rover.turnRight());
+    }
+
+    public void testGetX() {
+        Rover rover = new Rover(1, 1, 'N');
+        assertEquals(rover.getX(), 1);
+    }
+
+    public void testGetY() {
+        Rover rover = new Rover(1, 1, 'N');
+        assertEquals(rover.getY(), 1);
+    }
+
+    public void testGetOrientation() {
+        Rover rover = new Rover(1, 1, 'N');
+        assertEquals(rover.getOrientation(), 'N');
+    }
+}
