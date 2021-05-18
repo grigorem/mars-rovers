8fdbb9819f23d4af595b718cf72728dd3d2bc828
Marius Grigore <mariusgrygore@gmail.com>
2021-05-18 17:04:36 +0100

    Extended the functionality of Direction enum

--- src/main/java/org/grigore/Direction.java
+++ src/main/java/org/grigore/Direction.java
@@ -4,5 +4,25 @@ public enum Direction {
     NORTH,
     EAST,
     SOUTH,
-    WEST
+    WEST;
+
+    public Direction next() {
+        return values()[ordinal() + 1];
+    }
+
+    public Direction previous() {
+        return values()[ordinal() - 1];
+    }
+
+    public static Direction getFromChar(char c) {
+        if (c == 'N') {
+            return Direction.NORTH;
+        } else if (c == 'E') {
+            return Direction.EAST;
+        } else if (c == 'S') {
+            return Direction.SOUTH;
+        } else {
+            return Direction.WEST;
+        }
+    }
 }
