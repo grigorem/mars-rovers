f66667bc880fcd52873d784b4805d3d61be92db9
Marius Grigore <mariusgrygore@gmail.com>
2021-05-19 09:44:53 +0100

    Corrected tests

--- src/test/java/org/grigore/PlateauTest.java
+++ src/test/java/org/grigore/PlateauTest.java
@@ -7,6 +7,7 @@ public class PlateauTest {
 
     @Test
     public void testSingleton() {
+        Plateau.deleteInstance();
         Plateau plateau1 = Plateau.getInstance(1, 2);
         Plateau plateau2 = Plateau.getInstance();
         Assert.assertSame(plateau1, plateau2);
@@ -14,18 +15,21 @@ public class PlateauTest {
 
     @Test
     public void testGetxLimit() {
+        Plateau.deleteInstance();
         Plateau plateau = Plateau.getInstance(1, 2);
         Assert.assertEquals(plateau.getxLimit(), 1);
     }
 
     @Test
     public void testGetyLimit() {
+        Plateau.deleteInstance();
         Plateau plateau = Plateau.getInstance(1, 2);
         Assert.assertEquals(plateau.getyLimit(), 2);
     }
 
     @Test
     public void testConstructorWithArgumentsWithoutDelete() {
+        Plateau.deleteInstance();
         Plateau plateau1 = Plateau.getInstance(1, 2);
         Plateau plateau2 = Plateau.getInstance(2, 3);
         Assert.assertTrue(plateau2.getxLimit() == 1 && plateau2.getyLimit() == 2);
@@ -33,6 +37,7 @@ public class PlateauTest {
 
     @Test
     public void testConstructorWithArgumentsWithDelete() {
+        Plateau.deleteInstance();
         Plateau plateau1 = Plateau.getInstance(1, 2);
         Plateau.deleteInstance();
         Plateau plateau2 = Plateau.getInstance(2, 3);
