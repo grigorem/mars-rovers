9a48909353a14bf6bd79cc18cf2be95519b65ee9
Marius Grigore <mariusgrygore@gmail.com>
2021-05-20 12:08:50 +0100

    Added test for the rover boundaries

--- src/test/java/org/grigore/MovementControllerTest.java
+++ src/test/java/org/grigore/MovementControllerTest.java
@@ -108,4 +108,21 @@ public class MovementControllerTest extends TestCase {
                 rover2.getY() == 1 &&
                 rover2.getDirection() == Direction.EAST);
     }
+
+    public void testGoingOutOfPlateauBoundaries() {
+        Plateau.deleteInstance();
+        Plateau.getInstance(5, 5);
+        Rover rover = new Rover(1, 1, 'W');
+        MovementController.move(rover); // 0
+        MovementController.move(rover); // -1 -> should be 0
+        MovementController.turnLeft(rover);
+        MovementController.move(rover);
+        MovementController.move(rover);
+
+        assertTrue(
+                rover.getX() == 0 &&
+                rover.getY() == 0 &&
+                rover.getDirection() == Direction.SOUTH
+        );
+    }
 }
