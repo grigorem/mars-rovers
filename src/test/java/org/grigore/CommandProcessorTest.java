186f6b3651be26845db3a695ce7fc97ce56699e8
Marius Grigore <mariusgrygore@gmail.com>
2021-05-20 12:24:47 +0100

    Added testing for CommandProcessor

---
+++ src/test/java/org/grigore/CommandProcessorTest.java
@@ -0,0 +1,18 @@
+package org.grigore;
+
+import junit.framework.TestCase;
+
+public class CommandProcessorTest extends TestCase {
+
+    public void testProcessCommand() {
+        Plateau.deleteInstance();
+        String expected = "1 3 N" + System.lineSeparator() +
+                "5 1 E" + System.lineSeparator();
+        String input = "5 5" + System.lineSeparator() +
+                "1 2 N" + System.lineSeparator() +
+                "LMLMLMLMM" + System.lineSeparator() +
+                "3 3 E" + System.lineSeparator() +
+                "MMRMMRMRRM" + System.lineSeparator();
+        assertEquals(CommandProcessor.process(input), expected);
+    }
+}
