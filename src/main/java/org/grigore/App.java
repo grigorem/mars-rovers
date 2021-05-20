e6fa817dfa62a895a37f4173c3c859143e691e14
Marius Grigore <mariusgrygore@gmail.com>
2021-05-20 10:44:07 +0100

    Edited main class to receive user input (file or interactive)

--- src/main/java/org/grigore/App.java
+++ src/main/java/org/grigore/App.java
@@ -1,9 +1,19 @@
 package org.grigore;
 
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        StringBuilder input = new StringBuilder();
+        if (args.length > 0) {
+            input.append(UserInput.getFromUserFile(args[0]));
+        } else {
+            input.append(UserInput.getInputFromUser());
+        }
+
+        if (input.length() == 0) {
+            System.out.println("Input not valid");
+            return;
+        }
+
+        System.out.println(CommandProcessor.process(input.toString()));
     }
 }
