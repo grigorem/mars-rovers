39773229968ae09059175dbd24b58e2e62bacb03
Marius Grigore <mariusgrygore@gmail.com>
2021-06-01 17:40:15 +0100

    Added plateau initialization for each test

--- src/test/java/org/grigore/RoverTest.java
+++ src/test/java/org/grigore/RoverTest.java
@@ -7,6 +7,7 @@ public class RoverTest {
 
     @Test
     public void testMoveUp() {
+        Plateau.deleteInstance();
         Plateau plateau = Plateau.getInstance(100, 100);
         Rover rover = new Rover(1, 2, 'N');
         rover.move();
@@ -15,6 +16,8 @@ public class RoverTest {
 
     @Test
     public void testTurnLeft() {
+        Plateau.deleteInstance();
+        Plateau plateau = Plateau.getInstance(100, 100);
         Rover rover = new Rover(3, 4, 'N');
         rover.turnLeft();
         Assert.assertEquals(rover.getDirection(), Direction.WEST);
@@ -22,6 +25,8 @@ public class RoverTest {
 
     @Test
     public void testTurnRight() {
+        Plateau.deleteInstance();
+        Plateau plateau = Plateau.getInstance(100, 100);
         Rover rover = new Rover(1, 1, 'S');
         rover.turnRight();
         Assert.assertEquals(rover.getDirection(), Direction.WEST);
@@ -29,18 +34,24 @@ public class RoverTest {
 
     @Test
     public void testGetX() {
+        Plateau.deleteInstance();
+        Plateau plateau = Plateau.getInstance(100, 100);
         Rover rover = new Rover(1, 1, 'N');
         Assert.assertEquals(rover.getX(), 1);
     }
 
     @Test
     public void testGetY() {
+        Plateau.deleteInstance();
+        Plateau plateau = Plateau.getInstance(100, 100);
         Rover rover = new Rover(1, 1, 'N');
         Assert.assertEquals(rover.getY(), 1);
     }
 
     @Test
     public void testGetOrientation() {
+        Plateau.deleteInstance();
+        Plateau plateau = Plateau.getInstance(100, 100);
         Rover rover = new Rover(1, 1, 'N');
         Assert.assertEquals(rover.getDirection(), Direction.NORTH);
     }
